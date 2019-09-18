b24bd776bd8b795f170d49460478cdb831293303
tim ichiba <ichibat@gmail.com>
2019-09-19 08:15:44 +0900

    starting views

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 
+// paste this under var (const) express = require("express");
+const exphbs = require("express-handlebars");
+
 const app = express();
+
+//Handlebars middleware
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main"
+  })
+);
+app.set("view engine", "handlebars");
 
 //Index Route
 app.get("/", (req, res) => {
